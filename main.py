750708f0b74bc3a53b753b26bccb015754c7d39b
ihyaulhaq <138434786+ihyaulhaq@users.noreply.github.com>
2025-11-12 22:40:17 +0700

    Add print statment and sort result

--- main.py
+++ main.py
@@ -6,9 +6,20 @@ def get_book_text(filePath):
   
 
 if __name__ == '__main__':
-  print(get_num_words(get_book_text("./books/frankenstein.txt")))
   
-  print(get_num_chars(get_book_text("./books/frankenstein.txt")))
+  words_num = get_num_words(get_book_text("./books/frankenstein.txt"))
+  char_num = get_num_chars(get_book_text("./books/frankenstein.txt"))
+  result = sortedResult(get_num_chars(get_book_text("./books/frankenstein.txt")))
+  
+  # print_result(get_num_chars(get_book_text("./books/frankenstein.txt")))
   
   
+  print("============ BOOKBOT ============\n")
+  print("Analyzing book found at books/frankenstein.txt...\n")
+  print("----------- Word Count ----------\n")
+  print(words_num)
+  print("----------- Character Count ----------\n")
+  for x in result:
+    print(f"{x['char']}: {x['num']}")
+  print("============ END ============\n")
   
--- stats.py
+++ stats.py
@@ -3,15 +3,25 @@ def get_num_words(string):
   return f"Found {count} total words"
 
 def get_num_chars(string):
-  char_dist = {}
-  count = 0
+  char_dist = {}  
   for char in string.lower():    
     if char in char_dist:      
       char_dist[char] += 1      
     else:      
-      char_dist[char] = 1
-    count+=1
-    
-  return count, char_dist
+      char_dist[char] = 1        
+  return char_dist
+
+def sort_on(items):
+    return items['num']
+
+def sortedResult(items):
+  sorted_list = []    
   
+  for char, num in items.items():
+    {"char": "b", "num": 4868}
+    tmp = {"char": char , "num": num}
+    sorted_list.append(tmp)
+  
+  sorted_list.sort(reverse=True, key=sort_on)
+  return sorted_list
   
