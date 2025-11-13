6785ed78ecc220fc5c4a3a10da7dbaeeba8d29fb
ihyaulhaq <138434786+ihyaulhaq@users.noreply.github.com>
2025-11-13 10:19:04 +0700

    Add so the program can use command

--- main.py
+++ main.py
@@ -1,15 +1,25 @@
 from stats import *
+import sys
 
 def get_book_text(filePath):
   with open(filePath) as book:
     return book.read()
   
 
+
+
 if __name__ == '__main__':
   
-  words_num = get_num_words(get_book_text("./books/frankenstein.txt"))
-  char_num = get_num_chars(get_book_text("./books/frankenstein.txt"))
-  result = sortedResult(get_num_chars(get_book_text("./books/frankenstein.txt")))
+  if len(sys.argv) != 2:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)  
+
+
+  book_path = sys.argv[1]
+  
+  words_num = get_num_words(get_book_text(book_path))
+  char_num = get_num_chars(get_book_text(book_path))
+  result = sortedResult(get_num_chars(get_book_text(book_path)))
   
   # print_result(get_num_chars(get_book_text("./books/frankenstein.txt")))
   
